f81815372bee70470f0b314942ace5ab7f56ee65
Kutchrlapati, Sai Manoj Varma (Contractor) <saimanojvarma.kutchrlapati+mars@effem.com>
2025-11-28 16:08:39 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 module "vpc" {
-  source = "../modules/ec2"
+  source = "./modules/vpc"
   name = var.project_name
   cidr_block = var.vpc_cidr
   public_subnets = var.public_subnets
 }
 
 module "s3_backend" {
-  source = "../modules/S3"
+  source = "./modules/S3"
   bucket_name = "${var.project_name}-tfstate"
 }
 
 module "ec2" {
-  source = "../modules/ec2"
+  source = "./modules/ec2"
   name   = "${var.project_name}-srv"
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets_ids
